c84476f726fa54d3a481acea37c52b5f60ed2685
Adetayo <akinsanyaadetayo@gmail.com>
2020-05-10 09:26:44 +0100

    FEAT: Fix mobile navigation

--- dist/js/script.js
+++ dist/js/script.js
@@ -3,14 +3,10 @@ const img = document.querySelector('#img');
 const navBar = document.querySelector('#nav-bar');
 
 button.addEventListener('click', () => {
-  // console.log(navBar);
-
   showBtn = !showBtn;
-
-  // button.appendChild(img);
   if (showBtn == true) {
     img.src = './images/icon-close.svg';
-    navBar.style.display = 'block';
+    navBar.style.display = 'flex';
   } else {
     navBar.style.display = 'none';
     img.src = './images/icon-hamburger.svg';
